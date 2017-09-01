19e94648273726308412ead766655400a84a792e
Scottie Schneider <scott@laclave.co>
2017-08-31 21:54:35 -0500

    Update Added new comments to form.js
    Next steps:
    🙉 add in client side vvalidation for form
    🙉 Get the form information through jquery
    🙉 Review what I did since it's pretty fuzzy still

--- public/scripts/form.js
+++ public/scripts/form.js
@@ -3,6 +3,8 @@
 		//client side validation
 			//all fields in
 		//send post request with form laid out
+			// listen for status 200?
+			// if(res.status === 200, then use your routes to do something)
 
 $('#button').click(function(e){
 	e.preventDefault();
