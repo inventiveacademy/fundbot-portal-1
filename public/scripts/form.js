c305686ace8cddb63b1544b78356badf98003583
Scottie Schneider <scott@laclave.co>
2017-08-31 21:52:54 -0500

    Added jquery

---
+++ public/scripts/form.js
@@ -0,0 +1,10 @@
+// jquery onclick
+	// get form fields
+		//client side validation
+			//all fields in
+		//send post request with form laid out
+
+$('#button').click(function(e){
+	e.preventDefault();
+	alert('form submitted!')
+})
